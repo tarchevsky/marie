4c1a32165d592f57caf22afcf828fa3adfa5991c
Иван <i.tarchevsky@gmail.com>
2021-11-02 21:08:00 +0300

    Настройка скрипта почты и замена роликов на ютуб

--- js/script.js
+++ js/script.js
@@ -21,6 +21,17 @@ menuClose.onclick = function() {
 
 // Hamburger menu end
 
+// Tab start
+
+const ttoggle = document.querySelector('.target__toggle'),
+		topen = document.querySelector('.target__open');
+
+ttoggle.onclick = function() {
+	topen.classList.toggle('target__open--active');
+};
+
+// Tab end
+
 // Form script start
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -31,30 +42,6 @@ document.addEventListener('DOMContentLoaded', function () {
 		e.preventDefault();
 
 		let error = formValidate(form);
-
-		let formData = new FormData(form);
-		formData.append('image', formImage.files[0]);
-
-		if (error === 0) {
-			form.classList.add('_sending');
-			let response = await fetch('sendmail.php', {
-				method: 'POST',
-				body: formData
-			});
-			if (response.ok) {
-				let result = await response.json();
-				alert(result.message);
-				formPreview.innerHTML = '';
-				form.reset();
-				form.classList.remove('_sending');
-			} else {
-				alert("Ошибка");
-				form.classList.remove('_sending');
-			}
-		} else {
-			alert('Заполните обязательные поля');
-		}
-
 	}
 
 
@@ -66,67 +53,19 @@ document.addEventListener('DOMContentLoaded', function () {
 			const input = formReq[index];
 			formRemoveError(input);
 
-			if (input.classList.contains('_email')) {
-				if (emailTest(input)) {
-					formAddError(input);
-					error++;
-				}
-			} else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
-				formAddError(input);
-				error++;
-			} else {
-				if (input.value === '') {
-					formAddError(input);
-					error++;
-				}
+			if (input.classList.container('_email')) {
+
 			}
+
 		}
-		return error;
-	}
-	function formAddError(input) {
-		input.parentElement.classList.add('_error');
-		input.classList.add('_error');
-	}
-	function formRemoveError(input) {
-		input.parentElement.classList.remove('_error');
-		input.classList.remove('_error');
-	}
-	//Функция теста email
-	function emailTest(input) {
-		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-	}
-
-	//Получаем инпут file в переменную
-	const formImage = document.getElementById('formImage');
-	//Получаем див для превью в переменную
-	const formPreview = document.getElementById('formPreview');
-
-	//Слушаем изменения в инпуте file
-	formImage.addEventListener('change', () => {
-		uploadFile(formImage.files[0]);
-	});
-
-	function uploadFile(file) {
-		// провераяем тип файла
-		if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
-			alert('Разрешены только изображения.');
-			formImage.value = '';
-			return;
+		function formAddError(input) {
+			input.parentElement.classList.add('_error');
+			input.classList.add('_error');
 		}
-		// проверим размер файла (<2 Мб)
-		if (file.size > 2 * 1024 * 1024) {
-			alert('Файл должен быть менее 2 МБ.');
-			return;
+		function formRemoveError(input) {
+			input.parentElement.classList.remove('_error');
+			input.classList.remove('_error');
 		}
-
-		var reader = new FileReader();
-		reader.onload = function (e) {
-			formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
-		};
-		reader.onerror = function (e) {
-			alert('Ошибка');
-		};
-		reader.readAsDataURL(file);
 	}
 });
 
