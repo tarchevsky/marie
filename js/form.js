696650be2c2c3d5cc4796d39816a0082a5ca80d3
Иван <i.tarchevsky@gmail.com>
2021-11-03 16:56:48 +0300

    paths smart.php

--- js/form.js
+++ js/form.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
     e.preventDefault();
     $.ajax({
       type: "POST",
-      url: "mailer/smart.php",
+      url: ("mailer/smart.php", "../mailer/smart.php"),
       data: $(this).serialize()
     }).done(function () {
       $(this).find("input").val("");
