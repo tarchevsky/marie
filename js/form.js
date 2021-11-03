f26b2a4daacbb82ff0b86f42c1ab1234cf6940de
Иван <i.tarchevsky@gmail.com>
2021-11-03 15:35:44 +0300

    Новая форма, новая валидация формы, перед переносом на mamp локальный серве

--- js/form.js
+++ js/form.js
@@ -1,35 +1,20 @@
 $(document).ready(function(){
-
-   function validateForms(form){
-       $(form).validate({
-           rules: {
-               name: {
-                   required: true,
-                   minlength: 2
-               },
-               phone: "required",
-               email: {
-                   required: true,
-                   email: true
-               }
-           },
-           messages: {
-               name: {
-                   required: "Пожалуйста, введите свое имя",
-                   minlength: jQuery.validator.format("Введите {0} символа!")
-                 },
-               phone: "Пожалуйста, введите свой номер телефона",
-               email: {
-                 required: "Пожалуйста, введите свою почту",
-                 email: "Неправильно введен адрес почты"
-               }
-           }
-       });
-   };
-
-   validateForms('#consultation-form');
-   validateForms('#consultation form');
-   validateForms('#order form');
-
-   $('input[name=phone]').mask("+7 (999) 999-99-99");
+   $('.feed-form').validate({
+       rules: {
+    // simple rule, converted to {required:true}
+    name: "required",
+    // compound rule
+    email: {
+      required: true,
+      email: true
+    }
+  },
+  messages: {
+    name: "Пожалуйста, укажите своё имя",
+    email: {
+      required: "Нам нужна Ваша почта, чтобы связаться с Вами",
+      email: "Почта должна быть в формате: somemail@mail.ru or com"
+    }
+}
+   });
 });
