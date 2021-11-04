f2f3bffcb93e4ff5cde703292a82160954ff9542
Иван <i.tarchevsky@gmail.com>
2021-11-04 13:42:40 +0300

    Текст в главном блоке, запрет картинок, переставил ютуб под первым большим блоком текста

--- js/script.js
+++ js/script.js
@@ -113,3 +113,15 @@ $('.methodics').slick({
 })
 
 // Methodics end
+
+// Download none
+
+const img = document.getElementsByTagName('img');
+
+for(var i in img)
+{
+    img[i].oncontextmenu = function()
+    {
+        return false;
+    }
+}
